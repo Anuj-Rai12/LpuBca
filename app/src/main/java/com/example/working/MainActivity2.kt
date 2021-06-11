6ce425f41f0b5fabf2f172f34266a0031d844299
Anuj <anujrai04082002.0@gmail.com>
2021-06-11 10:57:41 +0530

    Added Some Manifest Permission

--- app/src/main/java/com/example/working/MainActivity2.kt
+++ app/src/main/java/com/example/working/MainActivity2.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.OnBackPressedCallback
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
