60b60637b6bde9311fc6be2da99144ce84cc0a7e
Anuj <anujrai04082002.0@gmail.com>
2021-06-17 11:53:27 +0530

    Working On Password reset Process status :Successfully

--- app/src/main/java/com/example/working/loginorsignup/forgertpass/ForgetPassWord.kt
+++ app/src/main/java/com/example/working/loginorsignup/forgertpass/ForgetPassWord.kt
@@ -1,25 +1,67 @@
 package com.example.working.loginorsignup.forgertpass
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.working.R
 import com.example.working.databinding.ForgetFragementBinding
+import com.google.android.material.snackbar.Snackbar
+import java.util.regex.Pattern
 
 
-class ForgetPassWord:Fragment(R.layout.forget_fragement) {
-    private lateinit var binding:ForgetFragementBinding
+class ForgetPassWord : Fragment(R.layout.forget_fragement) {
+    private lateinit var binding: ForgetFragementBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding= ForgetFragementBinding.bind(view)
+        binding = ForgetFragementBinding.bind(view)
         binding.nextBtn.setOnClickListener {
-            val action= ForgetPassWordDirections.actionForgetPassWordToMakeSelection()
-            findNavController().navigate(action)
+            val codePhone = "${binding.codePicker.selectedCountryCodeWithPlus}${binding.phoneText.text}"
+            if (binding.phoneText.text.toString().isBlank()
+                || binding.phoneText.text.toString().isEmpty() ||
+                binding.emailText.text.toString().isEmpty() ||
+                binding.emailText.text.toString().isBlank()
+            ) {
+                Snackbar.make(requireView(), "Please fill the Information", Snackbar.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+            if (!isValidPhone(codePhone)){
+                Snackbar.make(requireView(), "Please enter Correct Registered PhoneNo", Snackbar.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+            if (!isValidEmail(binding.emailText.text.toString()))
+            {
+                Snackbar.make(requireView(), "Please enter the Correct Email Address", Snackbar.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+            dir(codePhone,email = binding.emailText.text.toString())
         }
         binding.createacc.setOnClickListener {
-            val action=ForgetPassWordDirections.actionForgetPassWordToSignUpScreen()
+            val action = ForgetPassWordDirections.actionForgetPassWordToSignUpScreen()
             findNavController().navigate(action)
+        }
+    }
+
+    private fun dir(phone: String,email:String) {
+        val action = ForgetPassWordDirections.actionForgetPassWordToMakeSelection(email, phone)
+        findNavController().navigate(action)
+    }
+
+    private fun isValidPhone(phone: String): Boolean {
+        val phonetic = "^[+]?[0-9]{10,13}\$"
+        val pattern = Pattern.compile(phonetic)
+        return pattern.matcher(phone).matches()
+    }
+
+    private fun isValidEmail(target: CharSequence?): Boolean {
+        return if (target == null) {
+            false
+        } else {
+            Patterns.EMAIL_ADDRESS.matcher(target).matches()
         }
     }
 }
--- app/src/main/java/com/example/working/loginorsignup/forgertpass/MakeSelection.kt
+++ app/src/main/java/com/example/working/loginorsignup/forgertpass/MakeSelection.kt
@@ -1,21 +1,35 @@
 package com.example.working.loginorsignup.forgertpass
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.working.R
 import com.example.working.databinding.MkeFramgentBinding
 
 
-class MakeSelection:Fragment(R.layout.mke_framgent) {
+class MakeSelection : Fragment(R.layout.mke_framgent) {
     private lateinit var binding: MkeFramgentBinding
+    private val args: MakeSelectionArgs by navArgs()
+
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding= MkeFramgentBinding.bind(view)
-        binding.re1.setOnClickListener {
-            val action= MakeSelectionDirections.actionMakeSelectionToOTPScreen(null,null)
+        binding = MkeFramgentBinding.bind(view)
+        binding.apply {
+            emailDetail.text =
+                "The Password Reset Link will send to your Registered Email address ${args.email}"
+            phoneDetail.text = "The OTP will send to your Registered phone number ${args.phone}"
+        }
+        binding.phoneOption.setOnClickListener {
+            val action = MakeSelectionDirections.actionMakeSelectionToOTPScreen(null, null)
             findNavController().navigate(action)
+        }
+        binding.emailOption.setOnClickListener {
+            Toast.makeText(activity, "Doing", Toast.LENGTH_SHORT).show()
         }
     }
 }
